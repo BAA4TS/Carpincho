20601afa89d2a852ddbbe489b9d5cdd1c2df254d
BAA4TS <baa4ts@gmail.com>
2024-07-08 22:09:14 -0300

    update

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='Chozita',
-    version='1.5.6',
+    version='1.5.7',
     author='baaaaa',
     author_email='devbaats@gmail.com',
     description='Una mini librería de automatización de cifrado de archivos',
